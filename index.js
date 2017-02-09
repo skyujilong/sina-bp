f26a57803fc1b4ae8b1e3c2a798042f16b9efaf9
wa15aa <772751110@qq.com>
2017-02-09 09:20:16 +0800

    修改配置文件

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const dirBuild = require('./lib/path_test.js');
 const svnInit = require('./lib/svn_init');
 let _ = require('lodash');
 const svnConfig = require('./lib/svn_config');
+const svnUltimate = require('node-svn-ultimate');
 let argv = require('optimist').default({
     'homeDir': process.cwd(),
     'dirName': ''
@@ -38,7 +39,7 @@ function bulid(currentDir, list) {
 }
 //添加基础文件
 function addFile() {
-    let dir = path.resolve(__dirname, './lib/config');
+    let dir = path.resolve(__dirname, './config');
     _.forEach(fileConfg, function(val, key) {
         fs.readFile(path.join(dir, key), 'utf-8', (err, data) => {
             if (err) {
@@ -49,9 +50,9 @@ function addFile() {
                 
 
              data=data.replace('onlineSvnPathStr', onlinePath);
-             data=data.replace('tagPathStr', tagPath)
-             data=data.replace('tagNameStr', tagName)
-             data=data.replace('addPathStr', addDir)
+             data=data.replace('tagPathStr', tagPath);
+             data=data.replace('tagNameStr', tagName);
+             data=data.replace('addPathStr', addDir);
 
             }
             fs.writeFile(path.resolve(basePath, val, key), data, (err) => {
@@ -70,5 +71,8 @@ svnInit.init(function(){
        dirBuild(basePath,function(){
            bulid(basePath, dirConfig.subDir);
            addFile();
+           svnUltimate.commands.checkout( svnConfig.svnBaseUrl()+svnConfig.iteamName, basePath, function( err ) {
+               console.log( "Checkout complete" );
+           } );
        });
 });
