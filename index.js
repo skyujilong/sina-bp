561befe28f17cc744738c60a22e1776d5a2c5151
JiLong <skyujilong@163.com>
2017-02-20 14:40:20 +0800

    Revert "Merge pull request #2 from wa15aa/add_feature"
    This reverts commit 0449e98df3be50886bfcb6bf62b0aca8d8c28ba3, reversing
    changes made to 0844436db67266df23823c6288786d6d582e0ea1.
    
    回退错误版本

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const dirBuild = require('./lib/path_test.js');
 const svnInit = require('./lib/svn_init');
 let _ = require('lodash');
 const svnConfig = require('./lib/svn_config');
-const svnUltimate = require('node-svn-ultimate');
 let argv = require('optimist').default({
     'homeDir': process.cwd(),
     'dirName': ''
@@ -50,9 +49,9 @@ function addFile() {
 
 
              data=data.replace('onlineSvnPathStr', onlinePath);
-             data=data.replace('tagPathStr', tagPath);
-             data=data.replace('tagNameStr', tagName);
-             data=data.replace('addPathStr', addDir);
+             data=data.replace('tagPathStr', tagPath)
+             data=data.replace('tagNameStr', tagName)
+             data=data.replace('addPathStr', addDir)
 
             }
             fs.writeFile(path.resolve(basePath, val, key), data, (err) => {
@@ -71,14 +70,5 @@ svnInit.init(function(){
        dirBuild(basePath,function(){
            bulid(basePath, dirConfig.subDir);
            addFile();
-           svnUltimate.commands.checkout( svnConfig.svnBaseUrl()+svnConfig.iteamName, basePath, function( err ) {
-               console.log( "Checkout complete" );
-               svnUltimate.commands.add( basePath, {params:['* --force']}, function( err ) {
-                   console.log( "add complete" );
-                   svnUltimate.commands.commit( basePath, {params: [ '-m "Commit comment"' ]}, function( err ) {
-                       console.log( "commit complete" );
-                   } );
-               } );
-           } );
        });
 });
